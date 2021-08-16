bee342b1499efe462a306a2ab1fd4ae3ddd8fb38
kalayu_ftsum <kalayuftsumm@gmail.com>
2021-08-16 17:24:21 +0300

    add routings

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch ,Redirect} from 'react-router-dom';
+
+import NavBar from './components/navbar'
+import Home from './components/home'
+import Pokemon from './components/pokemon';
+import MyTeam from './components/myteam';
+
 
 function App() {
   return (
-    <div className="App">
-       Hello from app
-    </div>
+    <Router>
+    <NavBar />
+     <Switch>
+       <Route 
+            exact
+            path="/"
+            component={Home}
+       />
+
+       <Route 
+          exact
+          path="/pokemon/:pokemonName"
+          component={Pokemon}
+       />
+        <Route 
+          exact
+          path="/myteam"
+          component={MyTeam}
+       />
+
+     </Switch>
+    </Router>
   );
 }
 
